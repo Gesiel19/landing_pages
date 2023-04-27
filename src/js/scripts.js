4630addf459e8991d518140e1bea7e77ab7ddaa3
paola torres <paolandrea96@gmail.com>
2023-04-27 08:10:51 -0500

    Start script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,76 @@
 // escuchar el eveneto clic en los botones 
-document.querySelectorAll(".ButtonDown");
+const getContainerUl1 =document.querySelector(".liFeatures_div");
+
+document.addEventListener("click", (event)=>{
+console.log("sii");
+if(event.target.classList.contains("buttonDown1")){
+    console.log("aiuda")
+    const printDropDown= (container) => {
+        container.innerHTML = ""
+        container.innerHTML += `
+            <div>
+        <ul>
+        <li>  
+            <figure>
+               <img class="buttonDown" src="./assest/icon-todo.svg">
+           </figure> 
+           <a> Todo List  </a>
+        <li>  
+            <figure>
+               <img src="./assest/icon-calendar.svg">
+           </figure> 
+           <a> Calendar </a>
+        <li>  
+            <figure>
+               <img src="./assest/icon-reminders.svg">
+           </figure>
+           <a> Reminders </a>
+        <li>  
+            <figure>
+               <img src="./assest/icon-planning.svg">
+           </figure> 
+           <a> Planning </a> 
+            
+        </ul>
+        </div> `
+}
+printDropDown(getContainerUl1);
+}
+})
+document.addEventListener("click", (event)=>{
+    console.log("sii");
+    if(event.target.classList.contains("buttonDown2")){
+        console.log("aiuda2")
+        const printDropDown2= (container) => {
+            container.innerHTML = ""
+            container.innerHTML += `
+                <div>
+            <ul>
+            <li>  
+                <figure>
+                   <img class="buttonDown" src="./assest/icon-todo.svg">
+               </figure> 
+               <a> Todo List  </a>
+            <li>  
+                <figure>
+                   <img src="./assest/icon-calendar.svg">
+               </figure> 
+               <a> Calendar </a>
+            <li>  
+                <figure>
+                   <img src="./assest/icon-reminders.svg">
+               </figure>
+               <a> Reminders </a>
+            <li>  
+                <figure>
+                   <img src="./assest/icon-planning.svg">
+               </figure> 
+               <a> Planning </a> 
+                
+            </ul>
+            </div> `
+    }
+    printDropDown2(getContainerUl1);
+    }
+    })
+
